7959b933d8903154b5c7ea97cae3479153704ace
Richard Warfield <warfield@gmail.com>
2023-10-07 13:58:53 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, haskellNix, fenix }: {};
+}
