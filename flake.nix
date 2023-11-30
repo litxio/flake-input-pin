3c3be086a4e0e3cf60c1f4acf1a2ff1e92371117
Richard Warfield <richard@litx.io>
2023-11-30 19:48:41 +0700

    Free nixpkgs from haskellNix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
 
   inputs.fenix = {
     url = "github:nix-community/fenix";
