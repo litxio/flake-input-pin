ce7aa2dcb9ac08d5b7d07a79fb5f327a407a30da
Richard Warfield <richard@litx.io>
2023-12-01 11:14:45 +0700

    Hardcode rev for nixpkgs and haskell.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  # Pin these
+  inputs.nixpkgs.url = "nixpkgs/e12483116b3b51a185a33a272bf351e357ba9a99";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix/a9aa3790f542c023c198d01e31bdfecf4ea6e95a";
 
   inputs.fenix = {
     url = "github:nix-community/fenix";
